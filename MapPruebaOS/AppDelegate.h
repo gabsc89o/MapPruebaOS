a9e7c9f094ff4446a63e1e83b56f7095331572fe
gabsc89o <user138066@eu249.local>
2018-04-25 17:50:26 +0100

    Initial Commit

---
+++ MapPruebaOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapPruebaOS
+//
+//  Created by user138066 on 4/25/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
